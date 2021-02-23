e444cc8e0c22cf5424af7cd2aa429508d1dea180
Fazilabi <fazilag@code.edu.az>
2021-02-23 14:43:45 +0400

    11ci gun odev 1 tamamlandi

--- Business/Abstract/ICarService.cs
+++ Business/Abstract/ICarService.cs
@@ -16,6 +16,6 @@ namespace Business.Abstract
         IDataResult<List<Car>> GetCarsByBrandId(int brandId);
         IDataResult<List<Car>> GetCarsByColorId(int colorId);
         IDataResult<List<CarDetailDto>> GetCarDetails();
-        IDataResult<Car> GetById(int carId);
+        IDataResult<Car> GetById(int carId) ;
     }
 }
